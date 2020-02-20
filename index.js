f150fe779428db05314a7109e105c73bc56a3c64
William Barela <william.barela.dev@gmail.com>
2020-02-20 18:55:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Code your solution here
 
+const findMatching = (drivers, string) => {
+  return drivers.filter( driver => driver[0].toLowerCase)
+}
+
 const fuzzyMatch = (drivers, string) => {
   return drivers.filter( driver =>  driver[0] === string);
 };

606ba02d188ed6f40341c2e38f33c017aecc7c1c
William Barela <william.barela.dev@gmail.com>
2020-02-20 19:01:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here
 
 const findMatching = (drivers, string) => {
-  return drivers.filter( driver => driver[0].toLowerCase() === string.toLowerCase());
+  return drivers.filter( driver => driver.toLowerCase() === string.toLowerCase());
 };
 
 const fuzzyMatch = (drivers, string) => {
